af6d2d298e1020e87b20b6d511bb26a1a0ec56c1
James Hebden <james@hebden.net.au>
2014-09-21 08:02:19 +1000

    Updated gst python package

--- package/gstreamer/gst-python/gst-python.mk
+++ package/gstreamer/gst-python/gst-python.mk
@@ -8,7 +8,7 @@ GST_PYTHON_VERSION = 0.10.13
 GST_PYTHON_SOURCE = gst-python-$(GST_PYTHON_VERSION).tar.bz2
 GST_PYTHON_SITE = http://gstreamer.freedesktop.org/src/gst-python
 GST_PYTHON_INSTALL_STAGING = YES
-GST_PYTHON_DEPENDENCIES = host-pkgconf gstreamer gst-plugins-base host-python python
+GST_PYTHON_DEPENDENCIES = host-pkgconf gstreamer gst-plugins-base host-python python python-pygobject
 
 GST_PYTHON_CONF_EXTRA_OPT = \
 		--cross-prefix=$(TARGET_CROSS) \
